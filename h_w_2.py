bdd77b281b5d56948dedb4d43e9398f6f7244f8d
BML-M <161972511+BML-M@users.noreply.github.com>
2024-05-23 20:15:41 +0300

    Initial commit

---
+++ h_w_2.py
@@ -0,0 +1,7 @@
+homework_done = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_per_task = hours_spent / (homework_done - 2)
+print('Курс:', course_name, ', всего задач:', homework_done - 2, ', затрачено часов:', hours_spent,
+      ', среднее время выполнения', time_per_task, 'часа.'
+      )
---
+++ h_w_3.py
@@ -0,0 +1,10 @@
+name = 'Михаил'
+print('Name:', name)
+
+age = 30
+print('Age:', age)
+age += 1
+print('New Age:', age)
+
+is_student = True
+print('Is Student:', is_student)
