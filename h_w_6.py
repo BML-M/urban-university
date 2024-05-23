2fde5f05f92ddf5be76d2ea264630f70c009992a
BML-M <161972511+BML-M@users.noreply.github.com>
2024-05-23 22:20:38 +0300

    2305

--- h_w_6.py
+++ h_w_6.py
@@ -1,1 +1,29 @@
+# Работа со списками
+my_list = ['apple', 'banana', 'orange', 'kiwi']
 
+# Вывод списка на экран
+print('List:', my_list)
+
+# Вывод первого и последнего элементов списка
+print('First element:', my_list[0])
+print('Last element:', my_list[-1])
+
+# Вывод подсписка с третьего по пятый элементы
+print('Sublist:', my_list[2:5])
+
+# Изменение значения третьего элемента
+my_list[2] = 'grape'
+print('Modified list:', my_list)
+
+# Работа со словарями
+my_dict = {'apple': 'яблоко', 'banana': 'банан', 'orange': 'апельсин'}
+
+# Вывод словаря на экран
+print('Dictionary:', my_dict)
+
+# Вывод значения для заданного ключа
+print('Translation:', my_dict['orange'])
+
+# Изменение или добавление значения для заданного ключа
+my_dict['kiwi'] = 'киви'
+print('Modified dictionary:', my_dict)
