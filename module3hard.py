2339042d07f190788536b0790480b8988bbf4ef7
BML-M <161972511+BML-M@users.noreply.github.com>
2024-05-28 00:02:24 +0300

    2705

--- module3hard.py
+++ module3hard.py
@@ -1,20 +1,22 @@
 def calculate_structure_sum(*args):
     total_sum = 0
-
-    # Перебираем каждый элемент входных данных
     for arg in args:
-        if isinstance(arg, int):  # Если элемент является числом
+        if isinstance(arg, int):
             total_sum += arg
-        elif isinstance(arg, str):  # Если элемент является строкой
+        elif isinstance(arg, (list, tuple, set)):
+            sub_structure_sum = calculate_structure_sum(*arg)
+            total_sum += sub_structure_sum
+        elif isinstance(arg, dict):
+            for key, value in arg.items():
+                total_sum += calculate_structure_sum(key, value)
+        elif isinstance(arg, str):
             total_sum += len(arg)
-        elif isinstance(arg, (list, tuple)):  # Если элемент является списком или кортежем
-            total_sum += calculate_structure_sum(*arg)  # Рекурсивно вызываем функцию для элементов списка или кортежа
-        elif isinstance(arg, dict):  # Если элемент является словарем
-            total_sum += calculate_structure_sum(*arg.values())  # Рекурсивно вызываем функцию для значений словаря
-        elif isinstance(arg, set):  # Если элемент является множеством
-            total_sum += calculate_structure_sum(*arg)  # Рекурсивно вызываем функцию для элементов множества
+        elif isinstance(arg, tuple):
+            sub_structure_sum = calculate_structure_sum(*arg)
+            total_sum += sub_structure_sum
 
     return total_sum
+
 
 
 data_structure = [[1, 2, 3],
