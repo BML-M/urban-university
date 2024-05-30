1b9d2eb7f4bc0fd54af738475b3a433918731de2
BML-M <161972511+BML-M@users.noreply.github.com>
2024-05-30 20:46:11 +0300

    3005

---
+++ Module4/module_4_h_w_1.py
@@ -0,0 +1,13 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+
+    inner_function()
+
+
+# Вызываем функцию test_function
+test_function()
+
+# Попробуем вызвать inner_function вне функции test_function
+# Это вызовет ошибку, так как inner_function доступна только внутри test_function
+inner_function()
