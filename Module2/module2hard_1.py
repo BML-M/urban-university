2965d06d592542f74ee49c06a32f11d0d3db5888
BML-M <161972511+BML-M@users.noreply.github.com>
2024-05-30 19:38:40 +0300

    3005

--- Module2/module2hard_1.py
+++ Module2/module2hard_1.py
@@ -3,16 +3,21 @@ n = int(input("Введите число от 3 до 20: "))  # Запрашив
 numbers = list(range(1, n))  # Создаем список чисел от 1 до n
 pairs = []  # Создаем пустой список для пар чисел
 
-for i in range(len(numbers)):  # Перебираем все числа в списке numbers
-    for j in range(i + 1, len(numbers)):  # Перебираем все числа, начиная с i+1, в списке numbers
+i = 0
+while i < len(numbers):  # Пока не перебраны все числа в списке numbers
+    j = i + 1
+    while j < len(numbers):  # Пока не перебраны все числа, начиная с i+1, в списке numbers
         if numbers[i] + numbers[j] == n:
             pairs.append(str(numbers[i]))  # Добавляем первое число в пары
             pairs.append(str(numbers[j]))  # Добавляем второе число в пары
+            j += 1
             continue
 
-        if n % (numbers[i] + numbers[j]) == 0:
+        if n % (numbers[i] + numbers[j]) == 0:  # Проверяем условие: если n делится на сумму numbers[i] и numbers[j] без остатка
             pairs.append(str(numbers[i]))  # Добавляем первое число в пары
             pairs.append(str(numbers[j]))  # Добавляем второе число в пары
+        j += 1
+    i += 1
 
-result = "".join(pairs)  # Объединяем чи20сла в парах в одну строку
+result = "".join(pairs)  # Объединяем числа в парах в одну строку
 print(result)  # Выводим результат
