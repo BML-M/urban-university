e1b3616dab375b3f796a6c1d0e7fdfd045caa116
BML-M <161972511+BML-M@users.noreply.github.com>
2024-05-30 20:47:32 +0300

    3005

--- Module4/module_4_h_w_1.py
+++ Module4/module_4_h_w_1.py
@@ -9,5 +9,5 @@ def test_function():
 test_function()
 
 # Попробуем вызвать inner_function вне функции test_function
-# Это вызовет ошибку, так как inner_function доступна только внутри test_function
-inner_function()
+
+inner_function()  # Это вызовет ошибку, так как inner_function доступна только внутри test_function
