36346e5072c72ec7ff7991da3ddafa2ef6420c71
BML-M <161972511+BML-M@users.noreply.github.com>
2024-06-10 23:38:42 +0300

    1006

---
+++ Module_10/module_10_h_w_3.py
@@ -0,0 +1,40 @@
+import threading
+from threading import Lock
+
+
+class BankAccount:
+    def __init__(self):
+        self.balance = 1000
+        self.lock = Lock()  # Создаем объект блокировки
+
+    def deposit(self, amount):
+        with self.lock:  # Устанавливаем блокировку
+            self.balance += amount
+            print(f"Пополнено на {amount}, новый баланс: {self.balance}")
+
+    def withdraw(self, amount):
+        with self.lock:  # Устанавливаем блокировку
+            if self.balance >= amount:
+                self.balance -= amount
+                print(f"Снято {amount}, новый баланс: {self.balance}")
+            else:
+                print("Недостаточно средств для снятия")
+
+def deposit_task(account, amount):
+    for _ in range(5):
+        account.deposit(amount)
+
+def withdraw_task(account, amount):
+    for _ in range(5):
+        account.withdraw(amount)
+
+account = BankAccount()
+
+deposit_thread = threading.Thread(target=deposit_task, args=(account, 100))
+withdraw_thread = threading.Thread(target=withdraw_task, args=(account, 150))
+
+deposit_thread.start()
+withdraw_thread.start()
+
+deposit_thread.join()
+withdraw_thread.join()
