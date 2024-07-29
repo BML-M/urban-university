18381f1cb0df7e6b1313eba1fecbe7759751dc4f
BML-M <161972511+BML-M@users.noreply.github.com>
2024-07-29 23:50:46 +0300

    2907

---
+++ Module9/module_9_4_h_w.py
@@ -0,0 +1,35 @@
+from random import choice
+
+# Lambda-функция
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+
+# Создаем список совпадений по позициям
+compare = list(map(lambda x, y: x == y, first, second))
+print(compare)
+
+# Замыкание
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'a', encoding='utf-8') as f:
+            for data in data_set:
+                f.write(str(data) + '\n')
+    return write_everything
+
+# Пример использования функции замыкания
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+# Класс MysticBall
+class MysticBall:
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        return choice(self.words)
+
+# Пример использования класса MysticBall
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
